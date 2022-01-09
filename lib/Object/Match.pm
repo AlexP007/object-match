ba36aee794be86102dea88b2704ada1b56d0b49c
Alexander Panteleev <alexpan@cpan.org>
2022-01-09 17:09:52 +0300

    [i] geters for independent non-references @matches, %+, %-;

--- lib/Object/Match.pm
+++ lib/Object/Match.pm
@@ -1,7 +1,43 @@
+package Object::Match;
+
 use strict;
-use warnings 'FATAL';
+use warnings qw/FATAL/;
 use utf8;
 
-package Object::Match;
+use Carp;
+use Moo;
+use namespace::clean;
+
+tie my %nc, "Tie::Hash::NamedCapture";
+tie my %nca, "Tie::Hash::NamedCapture", all => 1;
+
+has regex => (
+    is       => 'ro',
+    required => 1,
+);
+
+has matches => (
+    is => 'rwp',
+);
+
+has names => (
+    is => 'rwp',
+);
+
+has names_a => (
+    is => 'rwp',
+);
+
+sub match {
+    my ($self, $string) = @_;
+
+    my @matches = $string =~ $self->regex;
+
+    $self->_set_matches(\@matches);
+    $self->_set_names({ %nc } );
+    $self->_set_names_a( { %nca } );
+
+    return scalar @matches;
+}
 
 1;
