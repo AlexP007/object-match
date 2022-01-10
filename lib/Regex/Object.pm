cd28b099886ae31bb8210163d30505a774ecbc88
Alexander Panteleev <alexpan@cpan.org>
2022-01-10 10:34:45 +0300

    [i] start implemetnig pod doc: * name * version * synopsis

--- lib/Regex/Object.pm
+++ lib/Regex/Object.pm
@@ -10,6 +10,8 @@ use Regex::Object::Match;
 use Moo;
 use namespace::clean;
 
+our $VERSION = '1.00';
+
 tie my %nc, "Tie::Hash::NamedCapture";
 tie my %nca, "Tie::Hash::NamedCapture", all => 1;
 
@@ -37,3 +39,31 @@ sub match {
 1;
 
 __END__
+
+# ABSTRACT: Regex object that solves problems with global variables side effects.
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Regex::Object - Regex object that solves problems with global variables side effects.
+
+=head1 VERSION
+
+version 1.00
+
+=head1 SYNOPSIS
+    use Regex::Object;
+
+    my $re = Regex::Object->new(regex  => qr/^\w{3}$/); # regex to match 3 letters words
+
+    print "matched\n" if $re->match('foo')->success;  # prints matched
+    print "matched\n" if $re->match('fooz')->success; # nothing
+
+    #### ---- ####
+
+
+
+=cut
