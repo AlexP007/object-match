02c95eb0b29180902d47d017990df36a3a0a2112
Alexander Panteleev <alexpan@cpan.org>
2022-01-10 13:58:07 +0300

    [i] continue implementing pod doc: * synopsis * description * methods

--- lib/Regex/Object.pm
+++ lib/Regex/Object.pm
@@ -62,8 +62,58 @@ version 1.00
     print "matched\n" if $re->match('foo')->success;  # prints matched
     print "matched\n" if $re->match('fooz')->success; # nothing
 
-    #### ---- ####
+    ######## ---- ########
 
+    $re = Regex::Object->new(regex  => qr/(?<name>\w+?) (?<surname>\w+)/); # named captures
 
+    my $result1 = $re->match('John Doe');
+    my $result2 = $re->match('Fill Anselmo');
+
+    # The main goal - both results have different named captured hashes
+    if ($result2->success) {
+        my $name    = $result2->named_captures->{name};
+        my $surname = $result2->named_captures->{surname};
+
+        print "Name: $name; Surname: $surname\n";
+    }
+
+    if ($result1->success) {
+        my $name    = $result1->named_captures->{name};
+        my $surname = $result1->named_captures->{surname};
+
+        print "Name: $name; Surname: $surname\n";
+    }
+
+=head1 DESCRIPTION
+
+This module was created for one certain goal: give you a level
+of isolation from perlre global variables.
+
+The Regex::Object supports qr// regex, so these modifiers
+could be used: m,s,i,x,xx,p,a,d,l,u,n.
+
+More about Perl Regex: L<perlre|https://perldoc.perl.org/perlre>.
+
+=head2 Regex::Object METHODS
+
+=head3 new(regex => $regex)
+
+    my $re = Regex::Object->new(regex  => qr/^\w{3}$/);
+
+Constructor: accept one parameter - qr// regex and returns new instance.
+
+=head3 regex()
+
+    my $regex = $re->regex;
+
+Returns regex that was passed to constructor earlier.
+
+=head3 match($string)
+
+    my $result = $re->match('foo');
+
+Execute regex matching and returns Regex::Object::Match result DTO.
+
+=head2 Regex::Object::Match METHODS
 
 =cut
